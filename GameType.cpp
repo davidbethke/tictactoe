883c249fd0749120a88e4255b57cc6c7f8c37b16
davidbethke <david.bethke@gmail.com>
2012-02-08 16:06:38 -0600

    setPlayerName order

--- GameType.cpp
+++ GameType.cpp
@@ -28,8 +28,10 @@ GameType::~GameType(void)
 void GameType::play()
 {
 	// set player names and marks
-	gameParams.players[0]->setMark(Mark::OH);
-	gameParams.players[1]->setMark(Mark::EX);
+	setPlayerNames(gameParams.players);
+	setPlayerOrder(gameParams.currentPlayerNumber,gameParams.nextPlayerNumber);
+	//gameParams.players[0]->setMark(Mark::OH);
+	//gameParams.players[1]->setMark(Mark::EX);
 	
 	gameParams.theBoard->clearBoard();
 	//cout << "Initial Board"<<endl;
@@ -38,7 +40,7 @@ void GameType::play()
 	while(!gameParams.theBoard->checkFull())
 	{
 		
-		cout << "Player Number:"<<gameParams.currentPlayerNumber<< " Move"<<endl;
+		cout << "Player Number:"<<gameParams.currentPlayerNumber<< " Name:"<< gameParams.players[gameParams.currentPlayerNumber]->getName()<<" Move"<<endl;
 		if(gameParams.players[gameParams.currentPlayerNumber]->move(gameParams.NUMROWS,gameParams.NUMCOLS,gameParams.theBoard))
 		{
 			
@@ -150,16 +152,23 @@ void setPlayerOrder(int& first, int& second)
 }
 void setPlayerNames(Player ** p) 
 {
-	string name0, name1;
+	string name0="", name1="";
 	// player 0
-	cout<<"Enter Player 0 Name"<<"default ("<<p[0]->getName()<<endl;
+	//cout<<"Enter Player 0 Name"<<" default ( "<<p[0]->getName()<<")"<<endl;
+	cout <<"Enter Player 0 Name, enter 0 for Computer"<<endl;
 	cin >> name0;
-	if(!name0.empty())
+	//cout <<endl;
+	//cout <<"Name:"<<name0<<endl;
+	if(name0 != "0")
 		p[0]->setName(name0);
-	cout<<"Enter Player 1 Name"<<"default ("<<p[1]->getName()<<endl;
-	cin >> name0;
-	if(!name1.empty())
-		p[1]->setName(name0);
+	//cout<<"Enter Player 1 Name"<<"default ( "<<p[1]->getName()<<")"<<endl;
+	cout <<"Enter Player 1 Name, enter 0 for Computer"<<endl;
+	cin >> name1;
+	if(name1 !="0")
+		p[1]->setName(name1);
+	//setMarks
+	p[0]->setMark(Mark::OH);
+	p[1]->setMark(Mark::EX);
 	
 }
 
