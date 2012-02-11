e4e33e7ba643fd0359b14449827f37f06e44d1b2
Dave Bethke <david.bethke@gmail.com>
2012-02-11 12:43:32 -0600

    testing jenkins/github

---
+++ jenkins.bat
@@ -0,0 +1,3 @@
+cd C:\Program Files\Jenkins\jobs\GameTicTacToeUnitTests\workspace;
+del C:\Program Files\Jenkins\jobs\GameTicTacToeUnitTests\workspace\test_detail.xml ;
+"D:\Users\dave\Documents\Visual Studio 2010\Projects\Game\Game\debug\Game.exe"
